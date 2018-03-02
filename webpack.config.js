a289cecaadd7c7983cabc473903f4f88fe8a4f12
Gerardo Junior <contato@gerardo-junior.com>
2018-03-02 04:23:06 -0300

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
-const webpack = require('webpack');
-const path = require('path');
-const merge = require('webpack-merge');
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+};

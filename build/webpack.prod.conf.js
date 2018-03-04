74e4af5c30b8e517d98708ed4239a0e27fa2b4fe
Gerardo Junior <contato@gerardo-junior.com>
2018-03-03 22:01:15 -0300

    Resolving to deploy travis

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -58,7 +58,8 @@ var webpackConfig = merge(baseWebpackConfig, {
       {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
-        ignore: ['.git*']
+        ignore: ['.git*'],
+        debug: true
       }
     ])
   ]

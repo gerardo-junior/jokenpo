e0c666232e54375e82f4e02a4e5dc15c4a20be47
Gerardo Junior <contato@gerardo-junior.com>
2018-03-05 07:24:59 -0300

    Finalizing effects and bug fixing with setTimeOut

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import Game from './components/Game'
+
 function ready (fn) {
   if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
     fn()
@@ -5,6 +7,8 @@ function ready (fn) {
     document.addEventListener('DOMContentLoaded', fn)
   }
 }
+
+window.game = null
 
 ready(function () {
   // Loading
@@ -17,7 +21,8 @@ ready(function () {
   // Configure choice menu
   document.querySelectorAll('div.choice-game .container .button').forEach(function (obj, id, nodeList) {
     obj.addEventListener('click', function (evt) {
-
+      // Instantiating object
+      window.game = new Game(obj.getAttribute('game'))
       document.querySelector('section.game').classList.add(obj.getAttribute('game'))
       setTimeout(function () {
         document.querySelectorAll('section.game button.option').forEach(function (obj, id, nodeList) {
@@ -43,6 +48,54 @@ ready(function () {
           }, 250)
         })
       })
+    })
+  })
+
+  var result = document.getElementById('result')
+  var showResultTimeout
+  var hideResultTimeout
+  var removeClassTimeout
+
+  document.querySelectorAll('section.game div.container button.option').forEach(function (obj, id, nodeList) {
+    obj.addEventListener('click', function (evt) {
+      nodeList.forEach(function (obj) {
+        obj.classList.remove('winner')
+        obj.classList.remove('loser')
+        obj.classList.remove('tie')
+      })
+
+      if (window.game !== null) {
+        var played = window.game.play(obj.getAttribute('play'))
+        if (!(played.tie instanceof Object)) {
+          document.getElementById(played.winner).classList.add('winner')
+          document.getElementById(played.loser).classList.add('loser')
+        } else {
+          document.getElementById(played.tie).classList.add('tie')
+        }
+        result.classList.add('hide')
+
+        clearTimeout(showResultTimeout)
+        clearTimeout(hideResultTimeout)
+        clearTimeout(removeClassTimeout)
+
+        showResultTimeout = setTimeout(function () {
+          result.classList.remove('hide')
+          result.innerHTML = played.message
+          result.classList.add('show')
+        }, 50)
+
+        hideResultTimeout = setTimeout(function () {
+          result.classList.remove('show')
+          result.classList.add('hide')
+          removeClassTimeout = setTimeout(function () {
+            nodeList.forEach(function (obj) {
+              obj.classList.remove('winner')
+              obj.classList.remove('loser')
+              obj.classList.remove('tie')
+            })
+          }, 3000)
+        }, 2000)
+      }
     })
   })
 })
